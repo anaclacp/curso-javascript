88073df36afae8f87d07d1085bb31a2fa26b6bbe
Ana Clara <anaclaracarnavalli@gmail.com>
2025-06-24 00:27:41 -0300

    Changes to be committed: 	new file:   javascript/atribuicao-ternario/atribuicao-e-ternario.md 	new file:   javascript/atribuicao-ternario/exercicio.js 	new file:   javascript/atribuicao-ternario/index.html 	new file:   javascript/atribuicao-ternario/script.js Aula de Ternário e Atribuição

---
+++ javascript/atribuicao-ternario/script.js
@@ -0,0 +1,19 @@
+// ! Atribuição 
+
+var numero = 20;
+
+// numero = numero + 10
+// console.log(numero);
+
+// ? para abreviar isso (não ter que repetir), basta:
+numero += 10;
+console.log(numero);
+
+// ! Ternário
+
+var idade = 19;
+var naoPossuiDiabetes = true
+var podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber)
+
+// ? condição ? true : false
